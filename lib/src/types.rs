a84da844a13ed2d198db033ee89a30574cea0f11
BigPapa314 <Big_Papa@gmx.net>
2022-01-13 12:46:02 +0100

    added pom parsing

---
+++ lib/src/types.rs
@@ -0,0 +1,90 @@
+//! A library for querying maven repositories.
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Project {
+    pub model_version: Option<String>,
+    pub group_id: Option<String>,
+    pub artifact_id: Option<String>,
+    pub version: Option<String>,
+    pub packaging: Option<String>,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub url: Option<String>,
+    pub licenses: Option<Licenses>,
+    pub developers: Option<Developers>,
+    pub scm: Option<Scm>,
+    pub dependencies: Option<Dependencies>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Licenses {
+    #[serde(rename = "license")]
+    pub values: Vec<License>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct License {
+    pub name: Option<String>,
+    pub url: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Developers {
+    #[serde(rename = "developer")]
+    pub values: Vec<Developer>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Developer {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub email: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Scm {
+    pub connection: Option<String>,
+    pub developer_connection: Option<String>,
+    pub url: Option<String>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Dependencies {
+    #[serde(rename = "dependency")]
+    pub values: Vec<Dependency>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Dependency {
+    pub group_id: Option<String>,
+    pub artifact_id: Option<String>,
+    pub version: Option<String>,
+    pub scope: Option<String>,
+    //    #[serde(rename = "exclusion")]
+    //pub exclusions: Vec<Exclusion>,
+    pub exclusions: Option<Exclusions>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Exclusions {
+    #[serde(rename = "exclusion")]
+    pub values: Vec<Exclusion>,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Exclusion {
+    pub group_id: Option<String>,
+    pub artifact_id: Option<String>,
+}
